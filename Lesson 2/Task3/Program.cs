01ed32045231722bbcf4c3bfd86dcda917e48493
Drowning <egorkros175@gmail.com>
2024-03-10 15:47:34 +1000

    Lesson2 - Task3

---
+++ Lesson 2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 9, 8, 5, 2, 4, 3, 5, 7, 3, 10 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    i = i + 1;
+}
