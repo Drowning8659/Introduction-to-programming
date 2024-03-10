682fa7aad9b18e08dd1b95eab34ad400d9781ef0
Drowning <egorkros175@gmail.com>
2024-03-10 16:18:37 +1000

    Lesson2 - Task4

---
+++ Lesson 2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 5;
+int[] array = { 20, 5, 99, 1520, 43 };
+int i = 0;
+int max = 0;
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine("Maximum is:");
+Console.WriteLine(max);
